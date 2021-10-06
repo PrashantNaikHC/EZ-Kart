207f89f306769ab6a946c27819f8bc11f6da16f6
prashant naik <real.hyper.clocked@gmail.com>
2021-10-06 15:21:39 +0530

    Adding Navigation from LoginFragment to RegistrationFragment

--- app/src/main/java/com/prashant/naik/ezcart/LoginFragment.kt
+++ app/src/main/java/com/prashant/naik/ezcart/LoginFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.prashant.naik.ezcart.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container,  false)
+        binding.signUpTextView.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegistrationFragment())
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return binding.root
     }
 
 }
