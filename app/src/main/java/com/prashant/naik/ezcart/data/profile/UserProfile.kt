62378748acedbcff409d95a3d4a4d1c8798dc1dc
prashant naik <real.hyper.clocked@gmail.com>
2021-10-12 20:54:06 +0530

    REFACTOR : UserProfile database name

--- app/src/main/java/com/prashant/naik/ezcart/data/profile/UserProfile.kt
+++ app/src/main/java/com/prashant/naik/ezcart/data/profile/UserProfile.kt
@@ -3,10 +3,11 @@ package com.prashant.naik.ezcart.data.profile
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.prashant.naik.ezcart.utils.Constants.Companion.USER_PROFILE_DATABASE
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity(tableName = "user_profiles")
+@Entity(tableName = USER_PROFILE_DATABASE)
 data class UserProfile (
     @PrimaryKey
     val userId: String,
