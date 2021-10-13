5e20b395b700e5456da2186a838b922bea1910eb
prashant naik <real.hyper.clocked@gmail.com>
2021-10-13 15:14:52 +0530

    Adding log out alert dialog

--- app/src/main/java/com/prashant/naik/ezcart/MainActivity.kt
+++ app/src/main/java/com/prashant/naik/ezcart/MainActivity.kt
@@ -3,6 +3,7 @@ package com.prashant.naik.ezcart
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var userProfileTextView: TextView
-    private var userName = ""
+    private lateinit var builder: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +36,10 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         userProfileTextView = navView.getHeaderView(0).findViewById(R.id.profile_text)
+        builder = AlertDialog.Builder(this)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController: NavController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -50,11 +53,31 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         navView.setNavigationItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.nav_logout -> {
-                    this.drawerLayout.closeDrawer(GravityCompat.START)
-                    Toast.makeText(this, "Logout successful", Toast.LENGTH_SHORT).show()
-                    navController.navigate(HomeFragmentDirections.actionHomeFragmentToLoginFragment())
+                    this@MainActivity.drawerLayout.closeDrawer(GravityCompat.START)
+                    builder.apply {
+                        setTitle(getString(R.string.log_out_title))
+                        setMessage(getString(R.string.log_out_message))
+                        setPositiveButton(
+                            R.string.logout_yes
+                        ) { _, _ ->
+                            Toast.makeText(
+                                this@MainActivity,
+                                getString(R.string.log_out_success),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            navController.navigate(HomeFragmentDirections.actionHomeFragmentToLoginFragment())
+                        }
+                        setNegativeButton(
+                            R.string.logout_no
+                        ) { dialog, _ ->
+                            dialog.cancel()
+                        }
+                    }
+                    val alertDialog: AlertDialog = builder.create()
+                    alertDialog.setCancelable(false)
+                    alertDialog.show()
                 }
                 R.id.cartFragment -> {
                     navController.navigate(R.id.cartFragment)
@@ -83,7 +106,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if(this.drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
             this.drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
