6fb5e23fe981cdc88b6c4615d4cc265db92cb9a2
prashant naik <real.hyper.clocked@gmail.com>
2021-10-20 11:11:01 +0530

    Adding test cases for CartViewModel

---
+++ app/src/test/java/com/prashant/naik/ezcart/ui/cart/CartViewModelTest.kt
@@ -0,0 +1,75 @@
+package com.prashant.naik.ezcart.ui.cart
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import com.prashant.naik.ezcart.MainCoroutineRule
+import com.prashant.naik.ezcart.data.Item
+import com.prashant.naik.ezcart.domain.FakeRepository
+import com.prashant.naik.ezcart.domain.usecases.CartUseCase
+import com.prashant.naik.ezcart.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class CartViewModelTest {
+
+    lateinit var repository: FakeRepository
+    lateinit var viewModel: CartViewModel
+
+    @get:Rule
+    var coroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup(){
+        repository = FakeRepository()
+        viewModel = CartViewModel(CartUseCase(repository))
+    }
+
+    @Test
+    fun `given user has items in cart, when on cart screen, should load the items in cart`() {
+        val items = mutableListOf(
+            Item("currency1","desc1","12-12-2021","item1",100, "1 Kg"),
+            Item("currency2","desc2","13-12-2021","item2",200, "2 Kg")
+        )
+        repository.cartItems = items
+        viewModel.getCartItems()
+        val result = viewModel.cartItemsList.getOrAwaitValue()
+        assertThat(items).isEqualTo(result)
+    }
+
+    @Test
+    fun `given user has items in cart, when user removes an item, should load updated items`() {
+        val initialItems = mutableListOf(
+            Item("currency1","desc1","12-12-2021","item1",100, "1 Kg"),
+            Item("currency2","desc2","13-12-2021","item2",200, "2 Kg")
+        )
+        val finalItems = mutableListOf(
+            Item("currency1","desc1","12-12-2021","item1",100, "1 Kg")
+        )
+        repository.cartItems = initialItems
+        viewModel.removeItemFromCart("item2")
+        viewModel.getCartItems()
+        val result = viewModel.cartItemsList.getOrAwaitValue()
+        assertThat(finalItems).isEqualTo(result)
+    }
+
+    @Test
+    fun `given user has items in cart, should get the total price of all items`() {
+        val items = mutableListOf(
+            Item("currency1","desc1","12-12-2021","item1",100, "1 Kg"),
+            Item("currency2","desc2","13-12-2021","item2",200, "2 Kg")
+        )
+        repository.cartItems = items
+        viewModel.getCartItems()
+        val result = viewModel.getItemTotalPrice()
+        assertThat(result).isEqualTo(300)
+    }
+}
