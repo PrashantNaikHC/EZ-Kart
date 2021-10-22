9c719aa2060c0046895c8127dc8db9eb63bf0e49
prashant naik <real.hyper.clocked@gmail.com>
2021-10-22 15:15:27 +0530

    adding dummy URL for the items

--- app/src/main/java/com/prashant/naik/ezcart/utils/ItemExt.kt
+++ app/src/main/java/com/prashant/naik/ezcart/utils/ItemExt.kt
@@ -22,3 +22,13 @@ fun Item.getMappedImageResource(): Int {
         }
     }
 }
+
+fun Item.getMappedImageResourceUrl(): String {
+    return when {
+        this.itemName.contains("Bread") -> "https://images.unsplash.com/photo-1598373182133-52452f7691ef?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGJyZWFkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        this.itemName.contains("Salt") -> "https://images.unsplash.com/photo-1612078877705-4fd95d3d82c2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHNhbHR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        this.itemName.contains("Milk") -> "https://images.unsplash.com/photo-1600788907416-456578634209?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWlsa3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        this.itemName.contains("Rice") -> "https://images.unsplash.com/photo-1615485737442-7d6ab9f64db9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmljZSUyMGJhZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        else -> "https://images.unsplash.com/photo-1598373182133-52452f7691ef?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGJyZWFkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    }
+}
