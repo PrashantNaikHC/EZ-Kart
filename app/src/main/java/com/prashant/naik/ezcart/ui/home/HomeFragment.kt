fb62c8bc77059555fca2c8ed724a178b9f8737a2
prashant naik <real.hyper.clocked@gmail.com>
2021-11-03 00:22:57 +0530

    Add user welcome note

--- app/src/main/java/com/prashant/naik/ezcart/ui/home/HomeFragment.kt
+++ app/src/main/java/com/prashant/naik/ezcart/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -53,6 +54,7 @@ class HomeFragment : Fragment() {
 
         try {
             mainViewModel.setCurrentUserProfile(args.userProfile)
+            Toast.makeText(requireActivity(), "Welcome back ${args.userProfile.firstName}!", Toast.LENGTH_SHORT).show()
         } catch (e:Exception) {
             Log.e("MainActivity", "onCreateView: ${e.message}")
         }
